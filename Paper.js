c163337c5b69293e7d32caaba304151e42189d47
ManasviRachi <70890654+ManasviRachi@users.noreply.github.com>
2020-10-23 15:49:08 +0530

    Update Paper.js

--- Paper.js
+++ Paper.js
@@ -1,7 +1,7 @@
 class Paper {
     constructor(x, y, r) {
       var options = {
-          isStatic:true,
+          isStatic:false,
           restitution:0.3,
           friction:0.5,
           density:1.2
