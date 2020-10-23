737126d8cb365b29df055152eadddd0000a6f992
ManasviRachi <70890654+ManasviRachi@users.noreply.github.com>
2020-10-23 15:47:47 +0530

    Update Dustbin.js

--- Dustbin.js
+++ Dustbin.js
@@ -7,10 +7,10 @@ class Dustbin {
         this.wallThickness=20;
         this.angle=0;
         this.bottomBody=Bodies.rectangle(this.x,this.y,this.dustbinWidth,this.wallThickness,{isStatic:true});
-        this.leftWallBody=Bodies.rectangle(this.x-this.dustbin/2,this.y-this.dustbinHeight/2,this.wallThickness);
+        this.leftWallBody=Bodies.rectangle(this.x-this.dustbin/2,this.y-this.dustbinHeight/2,this.wallThickness, this.dustbinHeight, {isStatic:false});
         Matter.Body.setAngle(this.leftWallBody,this.angle);
         
-        this.rightWallBody=Bodies.rectangle(this.x+this.dustbinWidth/2,this.y-this.dustbinHeight/2,this.wallThickness);
+        this.rightWallBody=Bodies.rectangle(this.x+this.dustbinWidth/2,this.y-this.dustbinHeight/2,this.wallThickness, this.dustbinHeight, {isStatic:false});
         Matter.Body.setAngle(this.rightWallBody,-1*this.angle);
         World.add(world,this.bottomBody);
         World.add(world,this.leftWallBody);
